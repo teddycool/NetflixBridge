5163378b824ce13d74baa36089e21d972e0671c2
teddycool <par@sundback.com>
2016-11-21 19:35:24 +0100

    Cleaned up some more..

--- NetflixBridgeConfig.py
+++ NetflixBridgeConfig.py
@@ -5,5 +5,5 @@ __author__ = 'teddycool'
 config =    { "IO": { "GreenLed": 19, "RedLed": 26, "RedButton": 13},
               "Button": {"Pressed": 0.5, "LongPressed": 3},
               "Main": {"CycleTime": 0.1},
-              "InternetDetection": {"URL": "http://www.google.com","TimeSlot":10}
+              "InternetDetection": {"URL": "http://www.google.com","TimeSlot":30}
                }
